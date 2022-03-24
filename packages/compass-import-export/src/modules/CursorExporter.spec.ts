1f70093f7b910d30c9b418f54fe48e1cc7f153a9
Leonardo Rossi <leonardo.rossi@gmail.com>
2022-03-24 12:50:28 +0100

    improved types for testing

--- packages/compass-import-export/src/modules/CursorExporter.spec.ts
+++ packages/compass-import-export/src/modules/CursorExporter.spec.ts
@@ -3,8 +3,8 @@ import { CursorExporter } from './CursorExporter';
 import type { CursorExporterOpts } from './CursorExporter';
 import { expect } from 'chai';
 import { spy } from 'sinon';
-import { MongoClient } from 'mongodb';
-
+import { MongoClient, ObjectId } from 'mongodb';
+import type { Collection, Document } from 'mongodb';
 class StringStream extends Writable {
   public output = '';
   constructor() {
@@ -18,107 +18,134 @@ class StringStream extends Writable {
     return this.output;
   }
 }
-
+const sampleDocuments = [
+  {
+    _id: new ObjectId('OKOKOKOKOKOK'),
+    first_name: 'John',
+    last_name: 'Appleseed',
+  },
+];
 describe('CursorExporter', function () {
   this.timeout(5000);
   let cursor;
   let client: MongoClient;
   let testCollectionName: string;
-  let collection;
+  let collection: Collection<Document>;
   let outputStream: StringStream;
-  afterEach(async function () {
-    await collection.drop();
-    await client.close();
-  });
 
-  beforeEach(async function () {
-    testCollectionName = 'cursor-exporter-test-' + Date.now().toString();
-    client = new MongoClient('mongodb://localhost:27018/test');
-    collection = client.db().collection(testCollectionName);
-    outputStream = new StringStream();
-    await client.connect();
-    await collection.insertMany([
-      {
-        _id: 'foo',
-        first_name: 'John',
-        last_name: 'Appleseed',
-      },
-    ]);
-    cursor = collection.find();
-  });
-  describe('Formatters', function () {
-    describe('CSV', function () {
-      it('should export all documents', async function () {
-        const opts: CursorExporterOpts = {
-          cursor,
-          type: 'csv',
-          output: outputStream,
-          columns: true,
-        };
-        const exporter = new CursorExporter(opts);
-        await exporter.start();
-        expect(outputStream.getOutput()).to.be.equal(
-          '_id,first_name,last_name\nfoo,John,Appleseed'
-        );
-      });
-      it('should export documents with only defined columns', async function () {
-        const opts: CursorExporterOpts = {
-          cursor,
-          type: 'csv',
-          output: outputStream,
-          columns: ['_id', 'first_name'],
-        };
-        const exporter = new CursorExporter(opts);
-        await exporter.start();
-        expect(outputStream.getOutput()).to.be.equal(
-          '_id,first_name\nfoo,John'
-        );
-      });
+  describe('Aggregations', function () {
+    beforeEach(async function () {
+      testCollectionName = 'cursor-exporter-test-' + Date.now().toString();
+      client = new MongoClient('mongodb://localhost:27018/test');
+      collection = client.db().collection(testCollectionName);
+      outputStream = new StringStream();
+      await client.connect();
+      await collection.insertMany(sampleDocuments);
+      cursor = collection.aggregate([{ $match: { first_name: 'John' } }]);
     });
-
-    describe('JSON', function () {
-      it('should export all documents', async function () {
-        const opts: CursorExporterOpts = {
-          cursor,
-          type: 'json',
-          output: outputStream,
-          columns: true,
-        };
-        const exporter = new CursorExporter(opts);
-        await exporter.start();
-        expect(JSON.parse(outputStream.getOutput())).to.be.deep.equal([
-          {
-            _id: 'foo',
-            first_name: 'John',
-            last_name: 'Appleseed',
-          },
-        ]);
-      });
+    afterEach(async function () {
+      await collection.drop();
+      await client.close();
     });
-  });
-
-  describe('Events', function () {
-    it('should emit "progress" event for each document', async function () {
-      const data = [
-        { first_name: 'Alice', _id: 'qwerty' },
-        { first_name: 'Bob', _id: 'asdfg' },
-        { first_name: 'Charlie', _id: 'zxcvb' },
-        { first_name: 'Diana', _id: '12345' },
-      ];
-      await collection.insertMany(data);
-      const onProgress = spy();
-      const exporter = new CursorExporter({
-        cursor: collection.find(),
+    it('should export AggregateCursor', async function () {
+      const opts: CursorExporterOpts = {
+        cursor,
         type: 'csv',
         output: outputStream,
         columns: true,
-      });
-      exporter.on('progress', onProgress);
+      };
+      const exporter = new CursorExporter(opts);
       await exporter.start();
-      expect(onProgress.callCount).to.equal(5);
+      expect(outputStream.getOutput()).to.be.equal(
+        '_id,first_name,last_name\n4f4b4f4b4f4b4f4b4f4b4f4b,John,Appleseed'
+      );
     });
   });
+  describe('Documents', function () {
+    beforeEach(async function () {
+      testCollectionName = 'cursor-exporter-test-' + Date.now().toString();
+      client = new MongoClient('mongodb://localhost:27018/test');
+      collection = client.db().collection(testCollectionName);
+      outputStream = new StringStream();
+      await client.connect();
+      await collection.insertMany(sampleDocuments);
+      cursor = collection.find();
+    });
+    afterEach(async function () {
+      await collection.drop();
+      await client.close();
+    });
+    describe('Formatters', function () {
+      describe('CSV', function () {
+        it('should export all documents', async function () {
+          const opts: CursorExporterOpts = {
+            cursor,
+            type: 'csv',
+            output: outputStream,
+            columns: true,
+          };
+          const exporter = new CursorExporter(opts);
+          await exporter.start();
+          expect(outputStream.getOutput()).to.be.equal(
+            '_id,first_name,last_name\n4f4b4f4b4f4b4f4b4f4b4f4b,John,Appleseed'
+          );
+        });
+        it('should export documents with only defined columns', async function () {
+          const opts: CursorExporterOpts = {
+            cursor,
+            type: 'csv',
+            output: outputStream,
+            columns: ['_id', 'first_name'],
+          };
+          const exporter = new CursorExporter(opts);
+          await exporter.start();
+          expect(outputStream.getOutput()).to.be.equal(
+            '_id,first_name\n4f4b4f4b4f4b4f4b4f4b4f4b,John'
+          );
+        });
+      });
 
-  it('should support aggregation results', async function () {});
-  it('should pause export', function () {});
+      describe('JSON', function () {
+        it('should export all documents', async function () {
+          const opts: CursorExporterOpts = {
+            cursor,
+            type: 'json',
+            output: outputStream,
+            columns: true,
+          };
+          const exporter = new CursorExporter(opts);
+          await exporter.start();
+          expect(JSON.parse(outputStream.getOutput())).to.be.deep.equal([
+            {
+              _id: { $oid: '4f4b4f4b4f4b4f4b4f4b4f4b' },
+              first_name: 'John',
+              last_name: 'Appleseed',
+            },
+          ]);
+        });
+      });
+    });
+    describe('Events', function () {
+      it('should emit "progress" event for each document', async function () {
+        const data = [
+          { first_name: 'Alice' },
+          { first_name: 'Bob' },
+          { first_name: 'Charlie' },
+          { first_name: 'Diana' },
+        ];
+        await collection.insertMany(data);
+        const onProgress = spy();
+        const exporter = new CursorExporter({
+          cursor: collection.find(),
+          type: 'csv',
+          output: outputStream,
+          columns: true,
+        });
+        exporter.on('progress', onProgress);
+        await exporter.start();
+        expect(onProgress.callCount).to.equal(5);
+      });
+    });
+    it('should pause export', function () {});
+  });
 });
